8cfc9334c99569b7966e03036e6dcaa15756bc52
colegiusto <cole.giusto@gmail.com>
2025-05-11 23:42:13 -0700

    unfucking the figs

--- Project1/control/controlAnalysis.m
+++ Project1/control/controlAnalysis.m
@@ -11,13 +11,13 @@ for w = 1:length(ws)
     
     u = sin(t*ws(w))*0.1;
     U = trapz(u.*exp(-1i*ws(w).*t));
-    r(w) = trapz((u-y).*exp(-1i*ws(w).*t))/U;
+    r(w) = trapz((y).*exp(-1i*ws(w).*t))/U;
 end
 
 
 subplot(2,1,1)
 semilogx(ws/2/pi, 20*log10(abs(r)))
-title("Closed Loop Frequency Response From Reference to Error")
+title("Closed Loop Frequency Response From Reference to Output")
 ylabel("Magnitude $[dB]$")
 grid on
 subplot(2,1,2)
@@ -25,4 +25,4 @@ semilogx(ws/2/pi, unwrap(angle(r))*180/pi)
 xlabel("Frequency $[Hz]$")
 ylabel("Phase $[\circ]$")
 grid on
-print(gcf, 'CLFreqRespError', '-dpng', '-r1200')
+print(gcf, 'CLFreqResp', '-dpng', '-r1200')
