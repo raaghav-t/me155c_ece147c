962fc7ca927cb7e4cb1d58c3cbf7c5a4ae29435c
TienXNguyen <epictien@gmail.com>
2025-05-11 22:18:14 -0700

    Inserted images as figures into latex doc

--- Project1/model_id/analysis_corr.m
+++ Project1/model_id/analysis_corr.m
@@ -11,13 +11,18 @@ for w = 1:length(ws)
     r(w) = sum(bary.*exp(-1i*ws(w).*t(1:end-1)))/U;
 end
 
-figure(2)
+figure(2);clf;
 hold on
 figure(1)
 subplot(2,1,1)
 semilogx(ws/2/pi, 20*log10(abs(r)))
+ylabel("Magnitude (dB)")
+grid on
 subplot(2,1,2)
 semilogx(ws/2/pi, unwrap(angle(r))*180/pi+360)
 ylim([90 360])
-
-
+ylabel("Phase (deg)")
+xlabel("Frequency (rad/s)")
+grid on;
+set(gcf,'Position',[0,0,1000,400]);
+exportgraphics(gcf,'./bodeid-corr.pdf');
--- Project1/model_id/analysis_param.m
+++ Project1/model_id/analysis_param.m
@@ -26,6 +26,9 @@ trfu = tfest(mdata, 3, 2);
 
 Gs = tf(trfu)
 %%
-figure(2)
+figure(2);clf;
 bode(Gs)
+grid on
 hold on
+set(gcf,'Position',[0,0,1000,400]);
+exportgraphics(gcf,'./bodeid-param.pdf');
