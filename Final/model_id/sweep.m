ac6e7a2f205be769eddfc93aa0c37d7e50932eb7
cole giusto <colegiusto@ad.ece.ucsb.edu>
2025-05-15 19:35:32 -0700

    first

---
+++ Final/model_id/sweep.m
@@ -0,0 +1,29 @@
+model = "Lab1";
+ws = logspace(-0.5,1,20)*2*pi;
+
+resp = zeros(length(ws),10000,3);
+for i = 1:length(ws)
+    omega = ws(i);
+    amp = 3;
+    set_param(model, 'Dirty', 'on')
+    rtwbuild(model)
+    disp(i)
+    disp(omega)
+
+    set_param(model, 'SimulationMode', 'external')
+    
+    set_param(model, 'SimulationCommand', 'connect')
+    pause(1)
+    
+    set_param(model, 'SimulationCommand', 'start')
+    pause(11)
+    
+    pos = cartPos.signals.values;
+    t = CartPos(:,1);
+
+    resp(i, :, 2:3) = pos;
+    resp(i,:,1) = t;
+end
+
+
+plot(resp(:,:,3))
